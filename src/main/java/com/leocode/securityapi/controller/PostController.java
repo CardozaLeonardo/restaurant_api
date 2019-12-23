06c11b2c5ceb6353d27e30b6d2ae6fe36e8eb00e
Leonardo <leonardoorozco69@gmail.com>
2019-12-23 15:49:08 -0600

    get posts by user endpoint

--- src/main/java/com/leocode/securityapi/controller/PostController.java
+++ src/main/java/com/leocode/securityapi/controller/PostController.java
@@ -6,11 +6,9 @@ import com.leocode.securityapi.models.request.PostRequest;
 import com.leocode.securityapi.repository.PostRepository;
 import com.leocode.securityapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import javax.servlet.http.HttpServletRequest;
@@ -34,6 +32,19 @@ public class PostController {
         return ResponseEntity.ok(posts);
 
         //return ResponseEntity.ok(posts);
+    }
+
+    @GetMapping("/get_posts_by_user/{user}")
+    public ResponseEntity<?> getPostsByUser(@PathVariable("user") int userId) {
+        Optional<User> user = userRepository.findById(userId);
+
+        if(!user.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        List<Post> posts = postRepository.getPostsByUser(user.get());
+
+        return ResponseEntity.ok(posts);
     }
 
     @PostMapping("/posts")
